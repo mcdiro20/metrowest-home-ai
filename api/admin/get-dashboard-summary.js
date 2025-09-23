d31b317b97d96f6397135950d573f033857d7a48
mcdiro20 <mattdiro@gmail.com>
2025-09-22 21:02:05 -0400

    Add files via upload

--- api/admin/get-dashboard-summary.js
+++ api/admin/get-dashboard-summary.js
@@ -1,5 +1,3 @@
-mport { createClient } from '@supabase/supabase-js';
-
 export default async function handler(req, res) {
   // Set CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
